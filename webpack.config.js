3eb1b4ff878d83e65b66c577714158f82fa1bad9
LinJuanmao <2211741172@qq.com>
2019-11-25 15:37:40 +0800

    github-battle

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = function(env,argv) {
+  const isEnvDevelopment = argv.mode === 'development' || !argv.mode;
+  const isEnvProduction = argv.mode === 'production';
+
+  return {
+    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    devtool: isEnvProduction ? 'source-map' : isEnvDevelopment && 'cheap-module-source-map',
+    entry: './src/index.js', 
+    output: {
+      filename:'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test:/\.css$/,
+          use:[
+            'style-loader',
+            'css-loader'
+          ]
+        },
+    ]
+    },
+    devServer:{
+      hot:true,
+      contentBase: path.resolve(__dirname, 'dist'),
+    },
+    plugins:[
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+  }
+};
